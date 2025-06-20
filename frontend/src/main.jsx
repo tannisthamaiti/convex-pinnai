34d6571cacbd01d37317f4492a274c1d4b43d652
tannisthamaiti <tannistha.iitkgp@gmail.com>
2025-06-20 12:59:52 +0000

    new convex changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ConvexProvider client={convex}>
+      <App />
+    </ConvexProvider>
   </React.StrictMode>,
 );
